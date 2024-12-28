b5158ce270cfb7e29409ceeaae0d72a31bd2e79f
윤겸 <yungyeom@yungyeom-ui-macbookair.local>
2024-12-28 21:37:00 +0900

    a

---
+++ app/src/main/java/com/example/photo1/PreActivity.java
@@ -0,0 +1,14 @@
+package com.example.myapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.photo1.R;
+
+public class PreActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pre_activity);
+    }
+}
